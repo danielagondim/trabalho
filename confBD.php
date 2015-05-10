2b475f9499b68b559d39b2aea2fc128f155df008
danielagondim <daniela.gondim@hotmail.com>
2015-05-10 11:09:24 -0300

    configuração banco de  dados
    banco de dadoss

--- confBD.php
+++ confBD.php
@@ -1,19 +1,31 @@
 <?php
  function conn_mysql(){
 
-   $servidor = '//${env.OPENSHIFT_MYSQL_DB_HOST}:${env.OPENSHIFT_MYSQL_DB_PORT}';
-   $porta = 3306;
-   $banco = "danielagondim";
-   $usuario = "adminsPbyDMf";
-   $senha = "utbCC8MTb-kW";
    
-      $conn = new PDO("mysql:host=$servidor;
-	                   port=$porta;
-					   dbname=$banco", 
-					   $usuario, 
-					   $senha,
-					   array(PDO::ATTR_PERSISTENT => true)
-					   );
-      return $conn;
+$servidor = '$sr1fm16sth.database.windows.net
+'   
+$porta = 1433;
+   
+$banco = "bancodani"
+   
+$usuario = "daniela";
+   
+$senha = "dani#2015"
+      
+$conn = new PDO("sqlsrv:server=$servidor;
+	                   
+		 port=$porta;
+					   
+		 dbname=$banco", 
+					   
+		 $usuario, 
+					   
+		 $senha,
+					  
+ array(PDO::ATTR_PERSISTENT => true)
+ );
+      
+return $conn;
    }
 ?>
+
