fb4b86cc8733c3bc0d1866323a0bb5f012ba3781
danielagondim <daniela.gondim@hotmail.com>
2015-05-10 23:30:20 -0300

    Banco de dados

--- confBD.php
+++ confBD.php
@@ -1,34 +1,19 @@
-
 <?php
  function conn_mysql(){
 
+   $servidor = 'us-cdbr-azure-northcentral-a.cleardb.com';
+   $porta = 3306;
+   $banco = "bancodadosdani";
+   $usuario = "beef55aa7ed688";
+   $senha = "a45c64a5";
    
-$servidor = 'us-cdbr-azure-northcentral-a.cleardb.com';
-     
-$porta = "3306";
-   
-$banco = "bancodadosdani";
-   
-$usuario = "beef55aa7ed688";
-   
-$senha = "a45c64a5";
-      
-$conn = new PDO("sqlsrv:server=$servidor;
-	                   
-		
-					   
-		 dbname=$banco", 
-					   
-		 $usuario, 
-					   
-		 $senha,
-					  
- array(PDO::ATTR_PERSISTENT => true)
- );
-      
-return $conn;
+      $conn = new PDO("mysql:host=$servidor;
+	                   port=$porta;
+					   dbname=$banco", 
+					   $usuario, 
+					   $senha,
+					   array(PDO::ATTR_PERSISTENT => true)
+					   );
+      return $conn;
    }
 ?>
-
-
-
